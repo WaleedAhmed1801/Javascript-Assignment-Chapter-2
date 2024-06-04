0cae6b0ae31c919432af5f00632367419cb6c1ba
Waleed Ahmed <w.ahmed1801@gmail.com>
2024-06-05 02:33:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var num1 = "Jhone Deo"
+alert(num1)
+
+var num2 = "15 years old"
+alert(num2)
+
+var num3 = "Certified mobile application Development"
+alert(num3)
+
+var num4 = "Pizza \nPizz \nPiz \nPi \nP"
+alert(num4)
+
+var num5 = "My email address is example@gmail.com"
+alert(num5)
+
+var num6 = "I'am trying to learn from the book smarter \n way to learn javascript"
+alert(num6)
+
+var num7 = "Yah ! I can write HTML content through Javascript"
+alert(num7)
+
+var num8 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(num8)
